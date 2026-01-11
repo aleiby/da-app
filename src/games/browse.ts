23d764e0c6f2c7f60446bd2d9982537502ec0a6b
Aaron Leiby <aleiby@gmail.com>
2026-01-10 21:57:36 -0800

    Revert "Browse: Sort owned cards before loaners in deck"
    This reverts commit 1fd75bc939e84be09a208a3edf6653ab3eadd017.

--- src/games/browse.ts
+++ src/games/browse.ts
@@ -1,108 +1,99 @@
-import { CardGame, ClickDeckArgs, ClickTableArgs } from '../cardgame';
-import { initDeck, getDecks, getDeckName, getShuffledDeck, CardDeckMap, getCard } from '../cards';
-import { revealCard } from '../cardtable';
-import { strict as assert } from 'assert';
+import { CardGame, ClickDeckArgs, ClickTableArgs } from "../cardgame";
+import { initDeck, getDecks, getDeckName, getShuffledDeck, CardDeckMap, getCard } from "../cards";
+import { revealCard } from "../cardtable";
+import { strict as assert } from "assert";
 
-export class Browse extends CardGame {
-  static get requiredPlayers() {
-    return 1;
-  }
-  getName() {
-    return 'Browse';
-  }
-  getMinPlayers() {
-    return Browse.requiredPlayers;
-  }
-  getMaxPlayers() {
-    return Browse.requiredPlayers;
-  }
+export class Browse extends CardGame
+{
+    static get requiredPlayers() {return 1;}
+    getName() {return "Browse";}
+    getMinPlayers() {return Browse.requiredPlayers;}
+    getMaxPlayers() {return Browse.requiredPlayers;}
 
-  async begin(initialSetup: boolean) {
-    if (!(await super.begin(initialSetup))) {
-      return false;
+    async begin(initialSetup: boolean) {
+        if (!await super.begin(initialSetup)) {
+            return false;
+        }
+
+        const names = initialSetup ? ['DeckA', 'Hand'] : await getDecks(this.tableId);
+        const decks = await Promise.all(names.map(name => initDeck(this.tableId, name)));
+
+        const dir: CardDeckMap = {};
+        decks.forEach(deck => dir[deck.name] = deck);
+
+        const hand = dir['Hand'];
+        assert(hand);
+
+        this.onClickDeck(async (args: ClickDeckArgs) => {
+            const name = args.deck;
+            const selected = args.selected;
+
+            // Add selected cards to deck.
+            if (selected && selected.length > 0) {
+                if (name in dir) {
+                    hand.moveIds(selected, dir[name], true);
+                }
+                return;
+            }
+
+            // Limit holding 24 cards.
+            if (await hand.numCards() >= 24 ) {
+                return;
+            }
+
+            // Draw card from deck.
+            const deck = dir[name];
+            if (deck) {
+                let card = await deck.drawCard(hand);
+                if (card != null) {
+                    revealCard(this.tableId, card);
+                }
+            }
+        });
+
+        this.onRightClickDeck(async (args: ClickDeckArgs) => {
+            const deck = dir[args.deck];
+            if (deck == null) {
+                return;
+            }
+
+            // Add selected cards to bottom of deck.
+            const selected = args.selected;
+            if (selected && selected.length > 0) {
+                hand.moveIds(selected, deck, deck == hand);
+                return;
+            }
+
+            // Right click to flip cards.
+            if (deck != hand) {
+                const id = await deck.peekId();
+                if (id != null) {
+                    deck.flipIds([id]);
+                    revealCard(this.tableId, await getCard(id));
+                }
+            }
+        });
+
+        this.onClickTable(async (args: ClickTableArgs) => {
+
+            // Create a new deck from selected cards.
+            const selected = args.selected;
+            if (selected && selected.length > 0) {
+                const deck = await initDeck(this.tableId, getDeckName(args.x, args.z));
+                hand.moveIds(selected, deck);
+                dir[deck.name] = deck;
+            }
+        });
+
+        const player = this.players[0];
+        const deck = dir['DeckA'];
+        assert(deck);
+
+        if (initialSetup) {
+            deck.add(await getShuffledDeck(player));
+        }
+
+        console.log("GO");
+        return true;
     }
-
-    const names = initialSetup ? ['DeckA', 'Hand'] : await getDecks(this.tableId);
-    const decks = await Promise.all(names.map((name) => initDeck(this.tableId, name)));
-
-    const dir: CardDeckMap = {};
-    decks.forEach((deck) => (dir[deck.name] = deck));
-
-    const hand = dir['Hand'];
-    assert(hand);
-
-    this.onClickDeck(async (args: ClickDeckArgs) => {
-      const name = args.deck;
-      const selected = args.selected;
-
-      // Add selected cards to deck.
-      if (selected && selected.length > 0) {
-        if (name in dir) {
-          hand.moveIds(selected, dir[name], true);
-        }
-        return;
-      }
-
-      // Limit holding 24 cards.
-      if ((await hand.numCards()) >= 24) {
-        return;
-      }
-
-      // Draw card from deck.
-      const deck = dir[name];
-      if (deck) {
-        const card = await deck.drawCard(hand);
-        if (card != null) {
-          revealCard(this.tableId, card);
-        }
-      }
-    });
-
-    this.onRightClickDeck(async (args: ClickDeckArgs) => {
-      const deck = dir[args.deck];
-      if (deck == null) {
-        return;
-      }
-
-      // Add selected cards to bottom of deck.
-      const selected = args.selected;
-      if (selected && selected.length > 0) {
-        hand.moveIds(selected, deck, deck == hand);
-        return;
-      }
-
-      // Right click to flip cards.
-      if (deck != hand) {
-        const id = await deck.peekId();
-        if (id != null) {
-          deck.flipIds([id]);
-          revealCard(this.tableId, await getCard(id));
-        }
-      }
-    });
-
-    this.onClickTable(async (args: ClickTableArgs) => {
-      // Create a new deck from selected cards.
-      const selected = args.selected;
-      if (selected && selected.length > 0) {
-        const deck = await initDeck(this.tableId, getDeckName(args.x, args.z));
-        hand.moveIds(selected, deck);
-        dir[deck.name] = deck;
-      }
-    });
-
-    const player = this.players[0];
-    const deck = dir['DeckA'];
-    assert(deck);
-
-    if (initialSetup) {
-      const cards = await getShuffledDeck(player);
-      // Sort owned cards (token_id >= 0) before loaners (token_id == -1)
-      cards.sort((a, b) => (b.token_id >= 0 ? 1 : 0) - (a.token_id >= 0 ? 1 : 0));
-      deck.add(cards);
-    }
-
-    console.log('GO');
-    return true;
-  }
 }
